374f53a9a075969021db44ea513c46dae51a9355
ChampPG <paul.gleason@mymail.champlain.edu>
2023-02-14 20:29:15 -0500

    auxcloner

--- SEC-480/cloneraux.ps1
+++ SEC-480/cloneraux.ps1
@@ -1,7 +1,15 @@
-################
-#  Cloner.ps1  #
-# Paul Gleason #
-################
+###################
+#  cloneraux.ps1  #
+#   Paul Gleason  #
+###################
+
+$connectCheck = $global:defaultviserver | Select-Object Name -ExpandProperty Name
+
+if ( $connectCheck -eq ""){
+    #Connect to vcenter
+    $vcenterdomain = Read-Host "Please enter domain for vcenter"
+    Connect-VISever -Server $vcenterdomain
+}
 
 # Show hosts
 Write-Host "--VM Host--"
@@ -27,24 +35,20 @@ Get-Datastore | Select-Object Name -ExpandProperty Name
 Write-Host "--------------"
 $dsName = Read-Host "Select Datastore you would like to use"
 
-# New VM name
-$newVMName = Read-Host "Please enter new VM Name"
+#  Get Clone name
+$cloneName = Read-Host "Enter the name for the clone"
 
-# Get VM
-$vm = Get-VM -Name $vmname
-# Get snapshot name
-$snapshot = Get-Snapshot -VM $vm -Name $snapshotName
 # Get vmhost
 $vmhost = Get-VMHost -Name $vmhostIP
+# Get snapshot name
+$snapshot = Get-Snapshot -VM $vm -Name $snapshotName
+# Get VM
+$vm = Get-VM -Name $vmname
 # Get Data Store
 $ds = Get-Datastore -Name $dsName
 # The name of the vm replaces {0}
-$linkedClone = “{0}.linked” -f $vm.name 
+$linkedClone = $cloneName
 # To create new linked clone
-$linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
-# Create full independent version from linked clone
-$newvm = New-VM -Name $newVMName -VM $linkedvm -VMHost $vmhost -Datastore $ds
-# Create snapshot of new vm
-$newvm | New-Snapshot -Name $snapshotName
-# Removed old link
-$linkedvm | Remove-VM -DeletePermanently
+$linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds\
+# Set Adapter
+$linkedvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName 480-WAN
