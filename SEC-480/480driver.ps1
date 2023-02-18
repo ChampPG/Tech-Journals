0af49c34593d48a1d0963d076adeb4bd4c7d391d
ChampPG <paul.gleason@mymail.champlain.edu>
2023-02-18 00:10:05 -0500

    Why must I have fat fingers

--- SEC-480/480driver.ps1
+++ SEC-480/480driver.ps1
@@ -1,7 +1,7 @@
 Import-Module '480-utils' -Force
 #Call the Banner Function
 480Banner
-$conf = Get-480Config -config_path = "/home/paul/Documents/Tech-Journals/SEC-480/480.json"
+$conf = Get-480Config -config_path "480.json"
 480Connect -server $conf.vcenter_server
 Menu
 
