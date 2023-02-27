acb7251f827b3c8a700aca79273f04f67e5e7807
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-27 10:45:25 -0500

    Update nginx.sh

--- SEC-350/Assessment Prep/nginx.sh
+++ SEC-350/Assessment Prep/nginx.sh
@@ -5,12 +5,22 @@ sudo apt-get install nginx -y
 sudo unlink /etc/nginx/sites/enabled/default
 cd /etc/nginx/sites-available/
 touch SEC350.conf
-curl https://raw.githubusercontent.com/ChampPG/Tech-Journals/main/SEC-350/Assessment%20Prep/nginx.conf > SEC350.conf
+
+/bin/cat << EOM >SEC350.conf
+http {
+    server {
+       location / {
+        root /data/www;
+      }
+    }
+}
+EOM
+
 ln -s /etc/nginx/sites-available/SEC350.conf /etc/nginx/sites-enabled/SEC350.conf
 
 cd /data/www/
 touch index.html
 
-/bin/cat << EOM >$FILE
+/bin/cat << EOM >index.html
   Paul Gleason nginx
 EOM
