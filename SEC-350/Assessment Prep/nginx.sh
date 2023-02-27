5b3e4b9493cffb8e74ab52fcca91c80a498da476
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-27 10:59:27 -0500

    Update nginx.sh

--- SEC-350/Assessment Prep/nginx.sh
+++ SEC-350/Assessment Prep/nginx.sh
@@ -2,25 +2,12 @@
 
 sudo apt-get install nginx -y
 
-sudo unlink /etc/nginx/sites/enabled/default
-cd /etc/nginx/sites-available/
-touch SEC350.conf
-
-/bin/cat << EOM >SEC350.conf
-http {
-    server {
-       location / {
-        root /data/www;
-      }
-    }
-}
-EOM
-
-ln -s /etc/nginx/sites-available/SEC350.conf /etc/nginx/sites-enabled/SEC350.conf
-
-cd /data/www/
+cd /var/www/html
 touch index.html
 
 /bin/cat << EOM >index.html
   Paul Gleason nginx
 EOM
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
