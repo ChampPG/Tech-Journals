7071c72813af31119d2f9d5d49f796dcf8c50e53
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-27 11:16:37 -0500

    Create ssh.ps1

---
+++ SEC-350/Assessment Prep/ssh.ps1
@@ -0,0 +1,16 @@
+#Requires -RunAsAdministrator
+# Install OpenSSH , does not install if "ssh" folder exists
+if (!(Test-Path "C:\ProgramData\ssh\ssh_host_rsa_key")) {
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+}
+
+# Start now and on startup
+Start-Service sshd
+Set-Service -Name sshd -StartupType Automatic
+
+# Add ssh-keys
+mkdir ssh-keys
+ssh-keygen.exe -f .\ssh-keys 
+
+# Recommend restart
+Write-Output "Recommended to restart machine now :)"
