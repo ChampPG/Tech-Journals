2bfa0b8ba735d5a26fd64667c4abd8576c080db8
Paul Gleason <71086240+ChampPG@users.noreply.github.com>
2023-03-04 14:03:11 -0500

    Update passwordTrav2Jump.sh

--- SEC-350/Assessment Prep/passwordTrav2Jump.sh
+++ SEC-350/Assessment Prep/passwordTrav2Jump.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-WINDOWSIP="172.16.150.50"
-JUMPUSER="jump-paul"
+WINDOWSUSER="paul"
+WINDOWSIP="10.0.17.27"
+JUMPUSER="paul-jump"
 JUMPIP="172.16.50.4"
 
-sftp paul@WINDOWSIP:ssh-keys.pub
-scp ssh-keys.pub paul@JUMPIP:travel.pub
+sftp $WINDOWSUSER@$WINDOWSIP:ssh-keys.pub
+scp ssh-keys.pub paul@$JUMPIP:travel.pub
 
-ssh paul@JUMPIP <<END
+ssh paul@$JUMPIP <<END
   sudo -i
   cat /home/paul/travel.pub >> /home/$JUMPUSER/.ssh/authorized_keys
 END
